9f036c347f3ced601e95dfa20ca8a8998a273564
cy4n1d3-p1x3l <mradulkumarsinghal2004@gmail.com>
2023-12-11 19:23:48 +0530

    Huff code is now dynamic

--- test/FFT.t.sol
+++ test/FFT.t.sol
@@ -86,7 +86,7 @@ contract FFTTest is Test {
         assertApproxEqAbs(sinVal, cosVal, 1e15);
     }
 
-    function test_fft_in_huff() public {
+    function test_fft_in_huff1() public {
         real_part.push(1 * 1e18);
         real_part.push(-1 * 1e18);
         real_part.push(0 * 1e18);
@@ -95,8 +95,8 @@ contract FFTTest is Test {
         complex_part.push(0 * 1e18);
         complex_part.push(1 * 1e18);
         complex_part.push(-1 * 1e18);
-        int256[4] memory re;
-        int256[4] memory im;
+        int256[] memory re;
+        int256[] memory im;
         (re, im) = fft_huff.fft(real_part, complex_part);
         assertApproxEqAbs(re[0], 0 * 1e18, 1e15);
         assertApproxEqAbs(im[0], 0 * 1e18, 1e15);
@@ -107,8 +107,47 @@ contract FFTTest is Test {
         assertApproxEqAbs(re[3], 0 * 1e18, 1e15);
         assertApproxEqAbs(im[3], -2 * 1e18, 1e15);
     }
+
+    function test_fft_in_huff2() public {
+        real_part.push(1 * 1e18);
+        real_part.push(-1 * 1e18);
+        real_part.push(0 * 1e18);
+        real_part.push(0 * 1e18);
+        real_part.push(1 * 1e18);
+        real_part.push(2 * 1e18);
+        real_part.push(3 * 1e18);
+        real_part.push(2 * 1e18);
+        complex_part.push(0 * 1e18);
+        complex_part.push(0 * 1e18);
+        complex_part.push(1 * 1e18);
+        complex_part.push(-1 * 1e18);
+        complex_part.push(0 * 1e18);
+        complex_part.push(1 * 1e18);
+        complex_part.push(3 * 1e18);
+        complex_part.push(0 * 1e18);
+        assertEq(real_part.length, 8);
+        int256[] memory re;
+        int256[] memory im;
+        (re, im) = fft_huff.fft(real_part, complex_part);
+        assertApproxEqAbs(re[0], 8 * 1e18, 1e15);
+        assertApproxEqAbs(im[0], 4 * 1e18, 1e15);
+        assertApproxEqAbs(re[1], -4.121 * 1e18, 1e15);
+        assertApproxEqAbs(im[1], 6.535 * 1e18, 1e15);
+        assertApproxEqAbs(re[2], 1 * 1e18, 1e15);
+        assertApproxEqAbs(im[2], -3 * 1e18, 1e15);
+        assertApproxEqAbs(re[3], 1.292 * 1e18, 1e15);
+        assertApproxEqAbs(im[3], 0.535 * 1e18, 1e15);
+        assertApproxEqAbs(re[4], 2 * 1e18, 1e15);
+        assertApproxEqAbs(im[4], 4 * 1e18, 1e15);
+        assertApproxEqAbs(re[5], 0.1213 * 1e18, 1e15);
+        assertApproxEqAbs(im[5], -0.5355 * 1e18, 1e15);
+        assertApproxEqAbs(re[6], -3 * 1e18, 1e15);
+        assertApproxEqAbs(im[6], -5 * 1e18, 1e15);
+        assertApproxEqAbs(re[7], 2.7071 * 1e18, 1e15);
+        assertApproxEqAbs(im[7], -6.5355 * 1e18, 1e15);
+    }
 }
 
 interface Wrapper {
-    function fft(int256[] memory, int256[] memory) external returns (int256[4] memory, int256[4] memory);
+    function fft(int256[] memory, int256[] memory) external returns (int256[] memory, int256[] memory);
 }
